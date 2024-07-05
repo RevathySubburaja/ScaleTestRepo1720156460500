eed3f92b71a1c05f39d2cca504a164f10d3d3e70
Revathy Subburaja <rsubburaja@vmware.com>
2024-07-05 10:44:22 +0530

    "Commit from automation"

---
+++ Test.java
@@ -0,0 +1,9 @@
+// This is a simple Java program.
+// FileName : "HelloWorld.java".
+ 
+class Test {
+    public static void main(String args[])
+    {
+        System.out.println("Scaleper Mock Project");
+    }
+}
